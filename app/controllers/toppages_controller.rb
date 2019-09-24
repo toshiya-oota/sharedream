68f2fedc921046cd0667ef958f1fa89ede968444
toshiya.oota <toshiya.1988.ota@gmail.com>
2019-09-24 05:21:28 +0000

    sharedori

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
+      @user = current_user
       @dream = current_user.dreams.build  # form_for 用
       @dreams = current_user.dreams.order('created_at DESC')
       @sharedreams = current_user.user_sharedreams.order('created_at DESC')
--- app/models/user.rb
+++ app/models/user.rb
@@ -37,8 +37,7 @@ class User < ApplicationRecord
   
   def self.search(search) #ここでのself.はUser.を意味する
     if search
-      where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
-      where(['self_content LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
+      where(['name LIKE ? or self_content LIKE ?', "%#{search}%","%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
     else
       all #全て表示。User.は省略
     end
